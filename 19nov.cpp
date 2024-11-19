5b6071d9d6b29552faf4f41c7f6bf512f600a663
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-11-19 11:13:28 +0530

    initial commit

---
+++ 19nov.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int num;
+    cout << " Enter Number" ;
+    cin >> num;
+    if ( ) {
+       cout << "" ; 
+    }
+    else if ( ) {
+        cout << "" ;
+    }
+    else {
+        cout << "" ;
+    }
+}
