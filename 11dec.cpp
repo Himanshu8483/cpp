d94c457dc01045fc8ce23d2ce2e0f0c6da785284
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-12-12 11:35:42 +0530

    all

--- 11dec.cpp
+++ 11dec.cpp
@@ -20,7 +20,7 @@
 using namespace std;
 int main() {
 int n=5;
-int a=3;
+int a=1;
 int r=3;
 for(int i=0;i<n;++i)
 {
---
+++ 12dec.cpp
@@ -0,0 +1,34 @@
+// number in letter 
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int num = 1234;
+    int rb = 0;
+    int dig = 0;
+    for (; num!=0; num/=10)
+    {
+        rb = rb*10 + (num%10);
+    }
+    // cout<< rb << endl;
+    for (; rb!=0; rb=rb/10) {
+        dig=rb%10;
+   
+    switch(dig){
+        case 1 : 
+        cout<< "one " ;
+        break;
+        case 2 : 
+        cout<< "two " ;
+        break;
+        case 3 : 
+        cout<< "three " ;
+        break;
+        case 4 : 
+        cout<< "four " ;
+        break;
+        default: cout << "wrong Input";
+        break;
+    }
+    }
+}
