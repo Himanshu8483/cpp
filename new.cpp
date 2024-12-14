2e1577e62a5839d9698af1814d248c5bfe8e2349
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-12-14 11:53:44 +0530

    all

--- new.cpp
+++ new.cpp
@@ -46,6 +46,42 @@ int main() {
     //     }
     //     cout<<endl;
     // }
+    // for(int r=1; r<=5; ++r) {
+    //     for(int c=1; c<=9; ++c){
+    //         if (c>=6-r && c<=4+r) {
+    //         cout<<"*";
+    //         }
+    //         else{
+    //         cout<<" ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int r=1; r<=5; ++r) {
+    //     for(int c=1; c<=9; ++c){
+    //         if (c>=r && c<=10-r) {
+    //         cout<<"*";
+    //         }
+    //         else{
+    //         cout<<" ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+    int a=0;
+    for(int r=1; r<=9; ++r) {
+        (r<=5) ? a++ : a--;
+
+        for(int c=1; c<=9; ++c){
+            if (c>=6-a && c<=4+a) {
+            cout<<"*";
+            }
+            else{
+            cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
 
 
 
