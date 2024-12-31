31512c8a91b1b9e7d48ce4c0eab39138a0507b0a
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2024-12-31 09:33:52 +0530

    all commit

--- arr.cpp
+++ arr.cpp
@@ -228,7 +228,7 @@
 // #include <iostream>
 // using namespace std;
 // int main() {
-//    // for only difference 1 value missing
+//    // find missing difference of one
 //     int arr[]= {1,2,3,5,7};
 //     for(int i=0;i<5;++i) {
 //         if(arr[i+1]-arr[i]>1){
@@ -257,7 +257,7 @@
 // #include <iostream>
 // using namespace std;
 // int main() {
-//    // value find in array
+//    // value find in array 
 //     int arr[]= {11,12,14,16,30};
 //     int target=16;      // yes
 //     // int target=21;      // no
@@ -268,6 +268,80 @@
 //             }
 //         }
 //         cout<<"no";
+// }
 
-
+#include <iostream>
+using namespace std;
+int main() {
+    // // remove any value from array 
+    // int arr[]={1,2,1,3,2};
+    // int remove=1;
+    // int j=0;
+    
+    // for(int i=0; i<5;++i)
+    // {
+    //     if(arr[i]!=remove){
+    //         arr[j]=arr[i];
+    //         ++j;
+    //     }
     // }
+    // for(int i=0; i<j;++i)   {
+    //     cout<<arr[i]<<" ";
+    // }
+
+// // print zeros at last 
+//     int arr[]= {1,0,3,2,0};
+//     int j= 0;
+//     for (int i=0;i<5; ++i)
+//     {
+//         if (arr[i]!=0){
+//             arr[j]=arr[i];
+//             ++j;
+//         }
+//     }
+//     for (int i=j;i<5; ++i){
+//         arr[i]=0;
+//     }
+//     for (int j=0;j<5;++j){
+//         cout<<arr[j]<<" ";
+//     }
+
+// // reverse 
+//     int arr[]={1,2,3,4,5};
+//     for (int i=0;i<5/2;++i){
+//         int temp;
+//         temp=arr[i];
+//         arr[i]=arr[5-i-1];
+//         arr[5-i-1]=temp;
+//     }
+//     for(int i=0; i<5; ++i)
+//     {
+//         cout<<arr[i]<< " ";
+//     }
+
+// // pallindrome check 
+//     int arr[]={1,2,3,2,1};
+//     for (int i =0; i<5/2; ++i){
+//         if(arr[i]!=arr[5-i-1])
+//         {
+//             cout<<"not pallindrome";
+//             return 0;
+//         }
+//     }
+//     cout<<"pallindrome";
+
+// // remove all duplicates
+    int arr[]={1,2,1,3,2};
+    for(int i=0; i<5;++i) {
+        for(int j=i+1; j<5; ++j) {
+            if(arr[i]==arr[j]){
+                arr[j]=-1;
+            }
+        }
+    }
+    for(int i=0; i<5; ++i){
+        if(arr[i]!=-1){
+            cout<<arr[i] << " ";
+        }
+    }
+}
--- function.cpp
+++ function.cpp
@@ -39,7 +39,7 @@
 //     {
 //         if(n<=1)
 //     {
-//         return n;        // base condition for break on recursion
+//         return n;        // base condition for break in recursion
 //     }
 //     return n +sum(n-1);      // sum of numbers
 //   // return n * sum(n-1);    // factorial of number 
@@ -48,3 +48,97 @@
 //     cout<< sum(10);
 // }
 
+// // fibonaci series 
+// #include <iostream>
+// using namespace std;
+// int fibo(int n){
+//     int ft=0;
+//     int st=1;
+//     int next=0;
+//     for(int i=0; i<n; ++i) {
+//         if(i==0){
+//             cout<<ft<<" ";
+//             continue;
+//         }
+//         if(i==1){
+//             cout<<st<< " ";
+//             continue;
+//         }
+//     next=ft+st;
+//     ft=st;
+//     st=next;
+//     cout<<next<<" ";
+//     }
+//     return 0;
+// }
+// int main (){
+//     fibo(10);
+// }
+
+// // fibonaci series write with another type 
+// #include <iostream>
+// using namespace std;
+// int fibo(int n);        // declare
+// int main (){
+//     fibo(10);
+// }
+
+// int fibo(int n){
+//     int ft=0;
+//     int st=1;
+//     int next=0;
+//     for(int i=0; i<n; ++i) {
+//         if(i==0){
+//             cout<<ft<<" ";
+//             continue;
+//         }
+//         if(i==1){
+//             cout<<st<< " ";
+//             continue;
+//         }
+//     next=ft+st;
+//     ft=st;
+//     st=next;
+//     cout<<next<<" ";
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// // Recursive Fibonacci function
+// int fibo(int n) {
+//     if (n == 0) return 0; // Base case for n=0
+//     if (n == 1) return 1; // Base case for n=1
+//     return fibo(n - 1) + fibo(n - 2);
+// }
+
+// int main() {
+//     int n = 10; // Example input
+//     cout << "at position " << n << " is " << fibo(n) << endl;      // at position 10 is 55
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+// Recursive function to calculate Fibonacci numbers
+int fibo(int n) {
+    if (n == 0) return 0; // Base case for n=0
+    if (n == 1) return 1; // Base case for n=1
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+// Function to print all Fibonacci numbers up to position n
+void printFibonacci(int n) {
+    for (int i = 0; i <= n; i++) {
+        cout << fibo(i) << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 10; // Example input
+    printFibonacci(n);      // 0 1 1 2 3 5 8 13 21 34 55 
+    return 0;
+}
