0afa9815c21d8ae03a5eca35793ba193856fab86
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-07-04 11:02:22 +0530

    Up

--- stu/practice.cpp
+++ stu/practice.cpp
@@ -1,16 +1,1 @@
 
-#include <iostream>
-using namespace std;
-
-class ConcreteExample {
-public:
-    void display()  {  // Concrete implementation of the abstract method
-        cout << "Concrete implementation." << endl;
-    }
-};
-int main() {
-    ConcreteExample obj;  // Create an object of the derived class
-    obj.display();        // Calls the overridden display() function
-    return 0;
-}
-
